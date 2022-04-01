5529150c3549fa5fdad8a1c9f596586d3de6f7fa
Eric Doczkat <edoczkat46@gmail.com>
2022-04-01 17:45:21 -0400

    work pls

--- src/components/reviews.tsx
+++ src/components/reviews.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const contactURL = () => {
-  window.open(
-    "https://go.thryv.com/site/6fxhrqsm3ixnu4f1/activity/dashboard",
-    "_self"
-  );
-};
+// const contactURL = () => {
+//   window.open(
+//     "https://go.thryv.com/site/6fxhrqsm3ixnu4f1/activity/dashboard",
+//     "_self"
+//   );
+// };
 
 function Reviews() {
   return (
